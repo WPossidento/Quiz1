4dd1821ce45823c3e98e202ce1f61552b260a11d
Macbook Pro <MacbookPro@Macbooks-MacBook-Pro.local>
2015-08-03 12:12:01 -0400

    Initial Commit

---
+++ Quiz1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Quiz1
+//
+//  Created by Macbook Pro on 8/3/15.
+//  Copyright (c) 2015 William Possidento. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

524f56b53b4e149f75d5169600de891e662cc19a
William Possidento <wpossidento@gmail.com>
2015-08-26 21:26:49 -0400

    A simple multiple-choice quiz app
    A simple multiple-choice chemistry quiz application. The questions are
    few and are drawn randomly, so there may be noticeable repetition, but
    this can be diminished with the addition of questions.

--- Quiz1/ViewController.swift
+++ Quiz1/ViewController.swift
@@ -9,17 +9,145 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var QuestionLabel: UILabel!
+    
+    @IBOutlet var Button1: UIButton!
+    @IBOutlet var Button2: UIButton!
+    @IBOutlet var Button3: UIButton!
+    @IBOutlet var Button4: UIButton!
+    
+    //@IBOutlet var LabelEnd: UILabel!
+    
+    @IBOutlet var Result: UILabel!
+
+    @IBOutlet var Next: UIView!
+    
+    var CorrectAnswer = String()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // Hide()
+        Result.text = ""
+        RandomQuestions()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    func RandomQuestions() {
+        var RandomNumber = arc4random() % 5
+        RandomNumber += 1 // Don't like starting with zero
+        
+        switch(RandomNumber) {
+            
+        case 1:
+            QuestionLabel.text = "What element is the lightest?"
+            Button1.setTitle("Hydrogen", forState: UIControlState.Normal)
+            Button2.setTitle("Helium", forState: UIControlState.Normal)
+            Button3.setTitle("Lithium", forState: UIControlState.Normal)
+            Button4.setTitle("Beryllium", forState: UIControlState.Normal)
+            CorrectAnswer = "1"
+            break
+            
+        case 2:
+            QuestionLabel.text = "Which of the following is liquid at room temperature?"
+            Button1.setTitle("Mercury", forState: UIControlState.Normal)
+            Button2.setTitle("Helium", forState: UIControlState.Normal)
+            Button3.setTitle("Silver", forState: UIControlState.Normal)
+            Button4.setTitle("Neon", forState: UIControlState.Normal)
+            CorrectAnswer = "1"
+            break
+            
+        case 3:
+            QuestionLabel.text = "What is the symbol for gold?"
+            Button1.setTitle("Gd", forState: UIControlState.Normal)
+            Button2.setTitle("Au", forState: UIControlState.Normal)
+            Button3.setTitle("Ag", forState: UIControlState.Normal)
+            Button4.setTitle("Hg", forState: UIControlState.Normal)
+            CorrectAnswer = "2"
+            break
+            
+        case 4:
+            QuestionLabel.text = "Which of the following is an alkali metal?"
+            Button1.setTitle("Silver", forState: UIControlState.Normal)
+            Button2.setTitle("Copper", forState: UIControlState.Normal)
+            Button3.setTitle("Lithium", forState: UIControlState.Normal)
+            Button4.setTitle("Boron", forState: UIControlState.Normal)
+            CorrectAnswer = "3"
+            break
+            
+        case 5:
+            QuestionLabel.text = "Which of the following is not a halogen?"
+            Button1.setTitle("Fluorine", forState: UIControlState.Normal)
+            Button2.setTitle("Chlorine", forState: UIControlState.Normal)
+            Button3.setTitle("Bromine", forState: UIControlState.Normal)
+            Button4.setTitle("Indium", forState: UIControlState.Normal)
+            CorrectAnswer = "4"
+            break
+            
+        default:
+            break
+        }
+        
+    }
+    
 
+    func Hide() {
+        // LabelEnd.hidden = true
+        Next.hidden = true
+    }
+    
+    
+    func unHide() {
+        // LabelEnd.hidden = false
+        Next.hidden = false
+    }
+    
+    
+    @IBAction func Button1Action(sender: AnyObject) {
+        unHide()
+        if(CorrectAnswer) == "1" {
+            Result.text = "Correct."
+        } else {
+            Result.text = "Wrong. Try again."
+        }
+    }
+    
+    @IBAction func Button2Action(sender: AnyObject) {
+        unHide()
+        if(CorrectAnswer) == "2" {
+            Result.text = "Correct."
+        } else {
+            Result.text = "Wrong. Try again."
+        }
+    }
+    
+    @IBAction func Button3Action(sender: AnyObject) {
+        unHide()
+        if(CorrectAnswer) == "3" {
+            Result.text = "Correct."
+        } else {
+            Result.text = "Wrong. Try again."
+        }
+    }
+    
+    @IBAction func Button4Action(sender: AnyObject) {
+        unHide()
+        if(CorrectAnswer) == "4" {
+            Result.text = "Correct."
+        } else {
+            Result.text = "Wrong. Try again."
+        }
+    }
+    
+    @IBAction func Next(sender: AnyObject) {
+        Result.text = ""
+        RandomQuestions()
+    }
 
 }
 
